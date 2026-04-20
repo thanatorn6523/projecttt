82b480085c897f542b22b03e60b79a0c9e00e4e0
thanatorn6523 <thanatorn.nam@spumail.net>
2026-04-20 15:46:07 +0700

    Update settings.py

--- project/myproject/myproject/settings.py
+++ project/myproject/myproject/settings.py
@@ -116,7 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'store/static'
+]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 INSTALLED_APPS = [
     'django.contrib.admin',       
     'django.contrib.auth',
